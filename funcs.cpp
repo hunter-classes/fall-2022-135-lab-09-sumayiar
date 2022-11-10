406e7a67ba035224710c526f50a11727cfa091bd
Sumayia Rashid <sumayiarashid@Sumayia-2.local>
2022-11-10 12:24:47 -0500

    finished orginal lab

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 #include "funcs.h"
+using namespace std; 
 
-// add functions here
+//Task A Function
+double length(Coord3D *p){
+    double length = sqrt(pow(p->x,2) + pow(p->y,2) + pow(p->z,2));
+    return length;
+}
+//Task B Function
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    if(length(p1) >= length(p2)){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+//Task C Function
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    if(length(p1) >= length(p2)){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+//Task E Functions
+Coord3D* createCoord3D(double x, double y, double z){
+    Coord3D* ppos = new Coord3D;
+    ppos->x = x;
+    ppos->y = y;
+    ppos->z = z;
+    return ppos;
+}
 
+void deleteCoord3D(Coord3D *p){
+    delete p;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
-int main()
-{
+int main(){
+    //Task A
+    Coord3D pointA = {10, 20, 30};
+    cout << length(&pointA) << endl; // would print 37.4166
+
+    //Task B
+    Coord3D pointP = {10, 20, 30};
+    Coord3D pointQ = {-20, 21, -22};
+
+    cout << "Address of P: " << &pointP << endl;
+    cout << "Address of Q: " << &pointQ << endl << endl;
+
+    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+   
+    cout << "ans = " << ans << endl; // So which point is farther?
+
+    //Task C
+    Coord3D pos = {0, 0, 100.0};
+    Coord3D vel = {1, -5, 0.2};
+
+    move(&pos, &vel, 2.0); // object pos gets changed
+    cout << pos.x << " " << pos.y << " " << pos.z << endl;
+    // prints: 2 -10 100.4
+
+    //Task D...
+
+    //Task E
+    double x, y, z;
+    cout << "Enter position: ";
+    cin >> x >> y >> z;
+    Coord3D *ppos = createCoord3D(x,y,z);
+    
+    cout << "Enter velocity: ";
+    cin >> x >> y >> z;
+    Coord3D *pvel = createCoord3D(x,y,z);
+
+    move(ppos, pvel, 10.0);
+
+    cout << "Coordinates after 10 seconds: " 
+         << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << endl;
+
+    deleteCoord3D(ppos); // release memory
+    deleteCoord3D(pvel);
   return 0;
 }
